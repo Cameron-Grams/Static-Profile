caa1c052ac5f1f5640e1b8e0ba6bfc7088b60e8b
Cameron <cameron.grams.2@gmail.com>
2018-05-14 10:24:11 -0400

    work on better project object

--- logic.js
+++ logic.js
@@ -1,8 +1,10 @@
-const endpoint = "https://thawing-bayou-67848.herokuapp.com/";
+// const endpoint = "https://thawing-bayou-67848.herokuapp.com/";
+const endpoint = "http://localhost:8080/";
+let allProjects = null;
 
-fetch( `${ endpoint }wake` )
-    .then( res => res.json() ) 
-    .then( json => console.log( json.message ) );
+// fetch( `${ endpoint }wake` )
+//    .then( res => res.json() ) 
+//    .then( json => console.log( json.message ) );
 
 var modalOverlay = document.querySelector( "#css-modalOverlay" );
 
@@ -25,9 +27,29 @@ const retrieveProject = ( index ) => {
         .catch( error => console.error('Error:', error ))
     };
 
+const callAllProjects = () => {
+        fetch( `${ endpoint }all-projects`, {
+            method: 'GET', 
+            headers: new Headers({
+              'Content-Type': 'application/json'
+            } )
+        } ).then( res => 
+            res.json() 
+        )
+        .then( json => {
+            console.log( 'here is the json: ', json );
+            allProjects = json;
+        } )
+        .catch( error => console.error('Error:', error ))
+    };
+
+callAllProjects();
+console.log( 'here are all projects: ', allProjects );
+
 // a function with Secondary Effects: Whhhaaaaa......?
 // As Master Yuan-Ma said: "If it works it is only partially stupid..." 
 const addProjectToDOM = ( projectObject ) => {
+        console.log( 'passed in: ', projectObject );
         var modalTitleNode = document.createElement( "h1" );
         var modalTitle = document.createTextNode( projectObject.title ); 
         modalTitleNode.className = "css-modalElement";
@@ -67,7 +89,11 @@ function toggleAndSend( e ) {
     const index = {
         index: indexValue
     };
-    retrieveProject( index );
+//    retrieveProject( index );
+    console.log( allProjects );
+    let currentProject = allProjects.projects[ indexValue ];  
+    console.log( 'resolves to this project: ', currentProject );
+    addProjectToDOM( currentProject );
 }
 
 modal.classList.toggle("closed");
